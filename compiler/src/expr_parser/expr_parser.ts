b5df5ed77d4917ba0a7c8b70d1d7e9ddead6364d
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-02-21 12:46:42 +0100

    fix: stripped quotes from source

--- compiler/src/expr_parser/expr_parser.ts
+++ compiler/src/expr_parser/expr_parser.ts
@@ -2,9 +2,11 @@ import * as ts from "typescript";
 import {createTransformer} from "./parse";
 
 export class ExpressionParser {
+    static instance: ExpressionParser = new ExpressionParser();
+
     parse(source: string, implicitVariables: string[]): ts.SourceFile {
         // Create a SourceFile object
-        const sourceFile = ts.createSourceFile("example.ts", source, ts.ScriptTarget.Latest, true, ts.ScriptKind.TS);
+        const sourceFile = ts.createSourceFile("example.ts", this.stripQuotes(source), ts.ScriptTarget.Latest, true, ts.ScriptKind.TS);
 
         // Apply the transformation
         const result = ts.transform(sourceFile, [createTransformer("ctx", implicitVariables)]);
@@ -18,4 +20,28 @@ export class ExpressionParser {
         // Return the transformed SourceFile node
         return transformedSourceFile;
     }
+
+    parseToExpression(source: string, implicitVariables: string[]) {
+        const statements =  this.parse(source, implicitVariables).statements
+
+        const stmt = statements[0];
+
+        if (!ts.isExpressionStatement(stmt)) {
+            throw new Error('Not a valid expression');
+        }
+
+        // @ts-ignore
+        return statements[0].expression;
+    }
+
+    stripQuotes(value: string): string {
+        if (
+            (value.startsWith("'") && value.endsWith("'")) ||
+            (value.startsWith('"') && value.endsWith('"'))
+        ) {
+            return value.slice(1, -1);
+        }
+        return value;
+    }
+
 }
