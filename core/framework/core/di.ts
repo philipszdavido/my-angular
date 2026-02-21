2c26f4ad9aaa3e71deeca543a2b02c5a947ba719
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-02-21 09:16:21 +0100

    feat: added functionality for dependency injection

---
+++ core/framework/core/di.ts
@@ -0,0 +1,19 @@
+import {getCurrentTNode, getLView} from "./state";
+
+// Look at current TNode
+//
+// Check bloom filter
+//
+// Search providers on node
+//
+// Search parent injectors
+//
+// Search root injector
+//
+// Throw if not found
+
+export function ɵɵdirectiveInject<T>(type: any) {
+    const lView = getLView();
+    const tNode = getCurrentTNode();
+
+}
